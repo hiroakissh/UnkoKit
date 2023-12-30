71b17e43e4e38562758f6b48b0cb21bbfa739ad4
hiroakissh <hiroaki.saito1014@gmail.com>
2023-12-30 18:12:15 +0900

    [add] 💩Stringの追加

--- Sources/UnkoKit/UnkoKit.swift
+++ Sources/UnkoKit/UnkoKit.swift
@@ -1,2 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct Unko {
+    public private(set) var unkoString = "💩"
+
+    public init(unkoString: String = "💩") {
+        self.unkoString = unkoString
+    }
+}
